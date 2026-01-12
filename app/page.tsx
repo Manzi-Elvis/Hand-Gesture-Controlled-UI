d07a950682e4ea29d913a7a99c42273082d274be
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-01-12 08:31:19 +0200

    Imported components

---
+++ app/page.tsx
@@ -0,0 +1,4 @@
+"use client"
+import { useState, useEffect, useCallback, useRef } from "react"
+import { CameraFeed } from "@/components/camera-feed"
+import { GestureGuide } from "@/components/gesture-guide"
